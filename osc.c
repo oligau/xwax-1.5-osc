c355972672bb2f56b3633cf92461cb9fe3b06c34
engine TK <engine@gas-werk.org>
2014-03-28 10:46:49 +0100

    BUGFIX xwax-client: sending disconnect to already disconnected deck caused player to stop

--- osc.c
+++ osc.c
@@ -439,9 +439,7 @@ int disconnect_handler(const char *path, const char *types, lo_arg ** argv,
     struct player *pl;
     de = &osc_deck[argv[0]->i];
     pl = &de->player;
-    
-    (void)player_toggle_timecode_control(pl);
-
+    pl->timecode_control = false;
     return 0;
 }
 
@@ -456,9 +454,7 @@ int reconnect_handler(const char *path, const char *types, lo_arg ** argv,
     struct player *pl;
     de = &osc_deck[argv[0]->i];
     pl = &de->player;
-    
-    (void)player_toggle_timecode_control(pl);
-
+    pl->timecode_control = true;
     return 0;
 }
 
